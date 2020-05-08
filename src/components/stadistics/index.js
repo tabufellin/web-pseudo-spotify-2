272c91f80256e1a00b8e0ffe3f8b622a8db5aa7e
tabufellin <dsgspersonal@gmail.com>
2020-05-07 22:14:18 -0600

    se hicieorn cambios a stadistics para que se vean mejor los componentes y ademas se agregaron botones que se miran y no se miran, tambien una nueva carpeta para convertir jsons en csv en functions que es una nueva carpeta

--- src/components/stadistics/index.js
+++ src/components/stadistics/index.js
@@ -4,10 +4,21 @@ import Query1 from '../query1/query1'
 import { createBrowserHistory } from 'history'
 import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css'
 import React, { useState, Fragment } from 'react'
 export const history = createBrowserHistory()
 
 const Stadistics = () => {
+
+    const bandera = false
+    const bandera1 = false
+    const bandera2 = false
+    const bandera3 = false
+    const bandera4 = false
+    const bandera5 = false
+    const bandera6 = false
+    const bandera7 = false
+    const bandera8 = false
 
     const [list, setList] = useState([])
     const [list2, setList2] = useState([])
@@ -18,208 +29,121 @@ const Stadistics = () => {
     const [list7, setList7] = useState([])
     const [list8, setList8] = useState([])
 
- /*   const onClick = () => {
-
-
-
-
-        fetch('http://localhost:3001/stadistics/1')
-        .then(function(response) {
-            return (response.json());
-        })
-        .then(function(data) {
-            console.log(data)            
-
-        });
-        fetch('http://localhost:3001/stadistics/2')
-        .then(function(response) {
-            return (response.json());
-        })
-        .then(function(data) {
-            console.log(data);
-        });
-        })
-        .then(function(data) {
-            console.log(data);
-        });
-        fetch('http://localhost:3001/stadistics/6')
-        .then(function(response) {
-            return (response.json());
-        })
-        .then(function(data) {
-            console.log(data);
-        });
-        fetch('http://localhost:3001/stadistics/7')
-        .then(function(response) {
-            return (response.json());
-        })
-        .then(function(data) {
-            console.log(data);
-        });
-        fetch('http://localhost:3001/stadistics/8')
-        .then(function(response) {
-            return (response.json());
-        })
-        .then(function(data) {
-        fetch('http://localhost:3001/stadistics/3')
-        .then(function(response) {
-            return (response.json());
-        })
-        .then(function(data) {
-            console.log(data);
-        });
-        fetch('http://localhost:3001/stadistics/4')
-        .then(function(response) {
-            return (response.json());
-            console.log(data);
-        })
-    }
-*/
-    return (
-    <div>
-
-    <button type="submit" className='btn btn-primary '
-     onClick={(e) => {
-        fetch('http://localhost:3001/stadistics/1')
+    const displayDataDB = (link, list, setList) => {
+        fetch(link)
         .then(function(response) {
             return (response.json());
         })
         .then(function(data) {
             console.log(data) 
-            setList([...list, ...data])           
+            console.log(list)
+            list.length = 0
+            setList([...list, ...data]) 
+          //  }
+                      
         });
-        
+    }
+
+    return (
+    <div>
+  {/*///////////////////////////////////////////////////////////////////*/ }
+
+    <button type="submit" className='btn btn-primary '
+     onClick={(e) => {
+        displayDataDB('http://localhost:3001/stadistics/1', list, setList)
+        document.getElementById("1").className="estoy-vivo"
+
+
      }}>
          Artistas con más álbumes publicados
         
     </button>
 
     <p>  {list.map((i, index) => <div key={index}> {i.name} {i.cantalbum} </div>)}   </p>
+    <button id="1" className="hola"> hola</button>
 
+    {/*///////////////////////////////////////////////////////////////////*/ }
+    
     <button type="submit"className='btn btn-primary ' onClick={(e) => {
-        fetch('http://localhost:3001/stadistics/2')
-        .then(function(response) {
-            return (response.json());
-        })
-        .then(function(data) {
-            console.log(data) 
-            setList2([...list2, ...data])           
-        });
+        displayDataDB('http://localhost:3001/stadistics/2', list2, setList2)
+        document.getElementById("2").className="estoy-vivo"
 
     }}>
         Cantidad de caciones por genero
     </button>
 
     <p> {list2.map((i, index) => <div key={index}> Genero: {i.name} : {i.cantcanciones} canciones</div>)} </p>
+    <button id="2" className="hola"> hola</button>
 
-
-
+    {/*///////////////////////////////////////////////////////////////////*/ }
 
     <button type="submit"className='btn btn-primary ' onClick={(e) => {
-        fetch('http://localhost:3001/stadistics/3')
-        .then(function(response) {
-            console.log("holaa")
-            return (response.json());
-        })
-        .then(function(data) {
-            console.log(data) 
-            setList5([...list5, ...data])           
-        });
-
+        displayDataDB('http://localhost:3001/stadistics/3', list5, setList5)
+        document.getElementById("3").className="estoy-vivo"
     }}>
         Total de duración de cada playlist
     </button>
 
     <p> {list5.map((i, index) => <div key={index}> playlist {i.name}: tiempo total en milisegundos: {i.sum}</div>)} </p>
+    <button id="3" className="hola"> hola</button>
+
+
+    {/*///////////////////////////////////////////////////////////////////*/ }
+
 
     <button type="submit"className='btn btn-primary ' onClick={(e) => {
-        
-        fetch('http://localhost:3001/stadistics/4')
-        .then(function(response) {
-            return (response.json());
-            
-
-        })
-        .then(function(data) {
-            console.log("hola")
-            console.log(data) 
-            setList4([...list4, ...data])           
-        });
-
+        displayDataDB('http://localhost:3001/stadistics/4', list4, setList4)
+        document.getElementById("4").className="estoy-vivo"
     }}>
         Canciones de mayor duración con su artista
     </button>  
 
     <p> {list4.map((i, index) => <div key={index}> Artista {i.name}, tiempo de duración: {i.milliseconds}  </div>)}</p>
+    <button id="4" className="hola"> hola</button>
 
-
-
+    {/*///////////////////////////////////////////////////////////////////*/ }
 
 
 
 
 
     <button type="submit"className='btn btn-primary ' onClick={(e) => {
-        
-        fetch('http://localhost:3001/stadistics/6')
-        .then(function(response) {
-            return (response.json());
-            
-
-        })
-        .then(function(data) {
-            console.log("hola")
-            console.log(data) 
-            setList3([...list3, ...data])           
-        });
-
+        displayDataDB('http://localhost:3001/stadistics/6', list3, setList3)
+        document.getElementById("5").className="estoy-vivo"
     }}>
         Promedio de duracion de canciones segun el genero
     </button>   
 
     <p> {list3.map((i, index) => <div key={index}> Genero: {i.name}, promedio de duración de sus canciones: {i.prom}  </div>)}</p>
+    <button id="5" className="hola"> hola</button>
 
+
+    {/*///////////////////////////////////////////////////////////////////*/ }
 
     <button type="submit"className='btn btn-primary ' onClick={(e) => {
-        
-        fetch('http://localhost:3001/stadistics/7')
-        .then(function(response) {
-            return (response.json());
-            
-
-        })
-        .then(function(data) {
-            console.log("hola")
-            console.log(data) 
-            setList6([...list6, ...data])           
-        });
-
+        displayDataDB('http://localhost:3001/stadistics/7', list6, setList6)
+        document.getElementById("6").className="estoy-vivo"
     }}>
         Cantidad de artistas diferentes por playlist
     </button> 
     
     <p> {list6.map((i, index) => <div key={index}> playlist: {i.name}, cantidad de artistas {i.count}  </div>)}</p>
+    <button id="6" className="hola"> hola</button>
 
+
+    {/*///////////////////////////////////////////////////////////////////*/ }
     <button type="submit"className='btn btn-primary ' onClick={(e) => {
-        
-        fetch('http://localhost:3001/stadistics/8')
-        .then(function(response) {
-            return (response.json());
-            
-
-        })
-        .then(function(data) {
-            console.log("hola")
-            console.log(data) 
-            setList7([...list7, ...data])           
-        });
-
+        displayDataDB('http://localhost:3001/stadistics/8', list7, setList7)
+        document.getElementById("7").className="estoy-vivo"
     }}>
         Artistas con más cantidad de generos musicales
     </button> 
 
     <p> {list7.map((i, index) => <div key={index}>{i.name}, con {i.count} generos musicales diferentes </div>)}</p>
+    <button id="7" className="hola"> hola</button>
 
+
+    {/*///////////////////////////////////////////////////////////////////*/ }
 
 
     </div>
@@ -227,7 +151,6 @@ const Stadistics = () => {
 )
 
 
-
-    }
+}
 
 export default Stadistics
